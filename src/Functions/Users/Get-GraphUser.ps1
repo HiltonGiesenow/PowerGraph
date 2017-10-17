bd045b5c03660cc3815bb824513270f3e080555a
Hilton Giesenow <hilton@giesenow.com>
2017-10-17 12:21:45 +0200

    Adding Get-GraphUser

---
+++ src/Functions/Users/Get-GraphUser.ps1
@@ -0,0 +1,30 @@
+﻿Function Get-GraphUser
+{
+    [CmdletBinding()]
+    Param(      
+        [Parameter(Mandatory = $true,
+                   ValueFromPipeline = $true,
+                   ValueFromPipelineByPropertyName = $true)]
+        [ValidateNotNullOrEmpty()]
+        [string[]]$UserId = @()
+    )
+
+    Process
+    {
+        foreach ($singleUserId in $UserId)
+        {
+            $emptyGuid = [guid]::Empty
+            if ([guid]::TryParse($singleUserId, [ref]$emptyGuid) -or $singleUserId.indexOf("@") -gt 0)
+            {
+                Write-Verbose "Getting record for user with id $singleUserId"
+
+                $uri = $global:PowerGraph_BaseUrl + "users/$singleUserId"
+                return Invoke-MSGraphRequest -Uri $uri
+            }
+            else
+            {
+                throw "Invalid User ID - $singleUserId"
+            }
+        }
+    }
+}
