1c483e3effe9e83266f2a3a7bab1d1f2cb460acb
Hilton Giesenow <hilton@giesenow.com>
2019-08-28 20:16:29 +0200

    adding parameterset to differentiate calls

--- src/PowerGraph365/Functions/Teams/Get-JoinedTeams.ps1
+++ src/PowerGraph365/Functions/Teams/Get-JoinedTeams.ps1
@@ -1,8 +1,8 @@
 ﻿Function Get-JoinedTeams
 {
     [CmdletBinding()]
-    Param(      
-        [Parameter(Mandatory = $false,
+    Param(
+        [Parameter(Mandatory = $true, ParameterSetName = "users/{id}/joinedTeams",
                    ValueFromPipelineByPropertyName = $true)]
         [ValidateNotNullOrEmpty()]
         [string[]]$UserId
@@ -11,13 +11,26 @@
     # if ($global:PowerGraph_AccessToken.TokenType -eq "Client Credentials")
     #     { Write-Warning "You cannot currently retrieve Plans for a Group using the Client Credentials authorization model. You might need to call Connect-MSGraph with a username and password first instead." }
 
-    if ($UserId -eq $null)
+    switch ($PsCmdlet.ParameterSetName)
     {
-        $uri = $global:PowerGraph_BaseUrl + "me/joinedTeams"
-    } else {
-        $uri = $global:PowerGraph_BaseUrl + "users/$UserId/joinedTeams"
-    }
+        "users/{id}/joinedTeams" {
 
+            #Todo: Validate UserId is valid for type
+
+            if ($global:PowerGraph_AccessToken.TokenType -eq "Password")
+                { Write-Warning "This operation is not support for this type of login ('Delegated') to the API. Please try using 'Connect-MSGraph' without a username and password." }
+
+            $uri = $global:PowerGraph_BaseUrl + "users/$UserId/joinedTeams"
+        }
+
+        default {
+            if ($global:PowerGraph_AccessToken.TokenType -eq "Client Credentials")
+                { Write-Warning "This operation is not support for this type of login ('Application') to the API. Please try using 'Connect-MSGraph' with a username and password." }
+
+            $uri = $global:PowerGraph_BaseUrl + "me/joinedTeams"
+        }
+    }
+    
     $return = Invoke-MSGraphRequest -Uri $uri
 
     return $return.value
