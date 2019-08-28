595a2da71114ec422175383183ecb65757fb905f
Hilton Giesenow <hilton@giesenow.com>
2019-08-28 21:51:36 +0200

    Fixing Get-Teams

--- src/PowerGraph365/Functions/Teams/Get-Teams.ps1
+++ src/PowerGraph365/Functions/Teams/Get-Teams.ps1
@@ -5,11 +5,12 @@ Function Get-Teams {
 
     if ($global:PowerGraph_BaseUrl.ToLower().Contains("beta") -eq $false)
     {
-        Write-Warning 'This query is currently only supported on the Beta endpoint - please call 'Connect-MSGrap' again with the following setting: -BaseUrl "https://graph.microsoft.com/beta/"'
+        Write-Warning 'This query/command is currently only supported on the Beta endpoint - please call 'Connect-MSGrap' again with the following setting: -BaseUrl "https://graph.microsoft.com/beta/"'
     } else {
-        $uri = $global:PowerGraph_BaseUrl + "/groups?$filter=resourceProvisioningOptions/Any(x:x eq 'Team')"
+        $uri = $global:PowerGraph_BaseUrl + "groups?`$filter=resourceProvisioningOptions/Any(x:x eq 'Team')"
 
         $return = Invoke-MSGraphRequest -Uri $uri
     }
     
+    return $return.value
 }
