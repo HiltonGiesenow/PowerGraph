b1961397f9aee254c6aa414b6f7d1ff73f4b7bf0
Hilton Giesenow <hilton@giesenow.com>
2019-08-28 21:52:38 +0200

    Fixing typos

--- src/PowerGraph365/Functions/Sites/New-SitePage.ps1
+++ src/PowerGraph365/Functions/Sites/New-SitePage.ps1
@@ -20,7 +20,7 @@ Function New-SitePage {
 
     if ($global:PowerGraph_BaseUrl.ToLower().Contains("beta") -eq $false)
     {
-        Write-Warning 'This query/command is currently only supported on the Beta endpoint - please call 'Connect-MSGrap' again with the following setting: -BaseUrl "https://graph.microsoft.com/beta/"'
+        Write-Warning 'This query/command is currently only supported on the Beta endpoint - please call 'Connect-MSGraph' again with the following setting: -BaseUrl "https://graph.microsoft.com/beta/"'
     } else {
 
         $body = (@{
